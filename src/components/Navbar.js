e36b9e492041f3dee84a02206f38d86aae44e719
Romit Sutariya <romitsutariya@gmail.com>
2023-04-04 23:09:20 +0530

    update tech stack

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -91,17 +91,6 @@ function NavBar() {
                 <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
               </Nav.Link>
             </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                href="https://romitsutariya.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
-
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/romitsutariya/Portfolio"
