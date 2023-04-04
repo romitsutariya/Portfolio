ba6b51273f7b26ef332201994fd8f67b4a824a7a
Romit Sutariya <romitsutariya@gmail.com>
2023-04-04 23:41:22 +0530

    updated instagram link

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/romitsutaria/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
