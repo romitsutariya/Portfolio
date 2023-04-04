e828997d0456ce6db025633448057e82a0599ea7
Romit Sutariya <romitsutariya@gmail.com>
2023-04-04 23:46:49 +0530

    update instagram user with https://www.instagram.com/i_romit_i/

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,7 +53,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/romitsutaria/"
+                href="https://www.instagram.com/i_romit_i/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/romitsutaria/"
+                  href="https://www.instagram.com/i_romit_i/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
