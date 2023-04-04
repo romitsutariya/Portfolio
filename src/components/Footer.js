25dc8fe15b9570d90e558126c0b795a0dc1a07b2
Romit Sutariya <romitsutariya@gmail.com>
2023-04-04 23:05:43 +0530

    updated about me page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                href="https://github.com/soumyajit4419"
+                href="https://github.com/romitsutariya"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://twitter.com/SutariyaRomit"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/romitsutariya/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
